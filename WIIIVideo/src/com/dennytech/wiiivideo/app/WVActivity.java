5c550ea6ca9ee80611376d07950e494188939666
Deng Jun <dengjun86@gmail.com>
2014-02-27 16:10:47 +0800

    improve list ui

--- WIIIVideo/src/com/dennytech/wiiivideo/app/WVActivity.java
+++ WIIIVideo/src/com/dennytech/wiiivideo/app/WVActivity.java
@@ -1,7 +1,17 @@
 package com.dennytech.wiiivideo.app;
 
+import android.widget.FrameLayout;
+
 import com.dennytech.common.app.CLActivity;
+import com.dennytech.wiiivideo.R;
 
 public class WVActivity extends CLActivity {
+	
+	@Override
+	public void setContentView(int layoutResID) {
+		super.setContentView(layoutResID);
+		FrameLayout rootFrame = (FrameLayout) findViewById(android.R.id.content);
+		rootFrame.setBackgroundResource(R.drawable.background_tabs);
+	}
 
 }
